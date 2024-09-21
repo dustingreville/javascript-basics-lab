086918a6b2201906c5e661c5c3fa2b7f50d637b1
dustingreville <dustingreville@gmail.com>
2024-09-21 01:26:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var studentName = "Dustin"
+var studentAge = 23
+var isEnrolled = true;
+
+
+let team = "Boston";
+let championships = 18
+let most = true;
+let least = false;
+const person = new Object();
+person .age = 36;
+person .name = "Mazulla"; 
+
+let starters = ['Holiday', 'Brown', 'Tatum', 'Porzingis', 'Horford']
+console.log (studentName, `is` ,studentAge, `and enrolled is` ,isEnrolled,)
+console.log (`His favourite team is` ,team, `and they have won` ,championships,`championships. This being the most ever is` ,!least,
+    `. They are coached by` ,person.name, `and the starters are` ,starters,
+)
+let x = 7
+let y = 21
+
+console.log (34*20, `is an example of an Arithmetic operator while` ,x!=y, 
+    `is an example of a comparison operator. I used a logical operator to show that the most is` ,!least,)
+
+
+
+
+
